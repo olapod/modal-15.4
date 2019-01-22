6415e8163fb596c0c0664d66b38a1ae672a4a6b0
Ola Podsiadlik <alecs@op.pl>
2019-01-22 13:33:49 +0100

    Finish Modal 15.4

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,1 @@
-"use strict";function Phone(o,i,n){this.brand=o,this.price=i,this.color=n}Phone.prototype.printInfo=function(){console.log("The phone brand is "+this.brand+", color is "+this.color+" and the price is "+this.price+".")};
+"use strict";function Phone(o,i,n){this.brand=o,this.price=i,this.color=n}Phone.prototype.printInfo=function(){console.log("The phone brand is "+this.brand+", color is "+this.color+" and the price is "+this.price+".")};var iPhone6S=new Phone("Apple",2250,"silver");iPhone6S.printInfo();
--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
 'use strict'; 
-function Phone(brand, price, color) {
+function Phone(brand, price, color, year, memory) {
     this.brand = brand;
 	this.price = price;
-	this.color = color;
+    this.color = color;
+    this.year = year;
+    this.memory = memory;
 }
 
 Phone.prototype.printInfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + ", memory is " + this.memory + " , the price is " + this.price + " and model year is " + this.year + ".");
+}
+var descriptionText = document.getElementById("description");
+
+Phone.prototype.wwwInfo = function() {
+    descriptionText.innerHTML = "The phone brand is " + this.brand + ", color is " + this.color + ", memory is " + this.memory + " , the price is " + this.price + " and model year is " + this.year + ".";
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
+var iPhone6S = new Phone("Apple", 2250, "silver", 2015, "32gb");
+var miMix2s = new Phone("Xiaomi", 1800, "black", 2018, "64gb");
+var mi8 = new Phone("Xiaomi", 1900, "blue", 2018, "128gb");
+var onePlus6t = new Phone("OnePlus", 2100, "silver", 2018, "64gb");
+
 iPhone6S.printInfo();
+miMix2s.printInfo();
+mi8.printInfo();
+onePlus6t.printInfo();
+
+
+miMix2s.wwwInfo();
+
+
